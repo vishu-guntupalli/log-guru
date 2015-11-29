f2afb11bf7d64d609a662f42af131fe8a35684f9
Vishu Guntupalli <iamno.16@gmail.com>
2015-11-29 09:55:18 -0600

    - Making file listen to new file when the log file is automatically rolled over. - Making browser scroll to the bottom on new lines.

--- app.js
+++ app.js
@@ -17,6 +17,13 @@ app.use('/swag/socket.io', express.static(__dirname + '/node_modules/socket.io-c
  * Configuration
  */
 
+fs.watch('/log4j/epi-test-webapp.log.1', function(path, stats){
+   console.log('file name changed');
+   follower = null;
+    follower = follow(__logFile);
+    watchForLineChange();
+});
+
 app.get('/', function(req, res){
     res.sendfile('index.html');
 });
@@ -24,10 +31,14 @@ app.get('/', function(req, res){
 io.on('connection', function(socket){
     var originalData = null;
     console.log('a user connected');
+    watchForLineChange();
+});
+
+var watchForLineChange = function () {
     follower.on('line', function(filename, line) {
         io.emit('message', line);
     });
-});
+}
 
 http.listen(3000, function(){
     console.log('listening on *:3000');
