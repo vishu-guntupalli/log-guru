d968b97c53be5c45efec6779f9d752b0fd9bea65
Vishu Guntupalli <iamno.16@gmail.com>
2016-01-03 13:33:23 -0600

    - Persisting everything in the database as embedded arrays. - Fixing variables.

--- logPersist.js
+++ logPersist.js
@@ -9,7 +9,14 @@ var mongoClient = require('mongodb').MongoClient;
 var mongoDbUrl =  'mongodb://localhost:27017/log-guru';
 var logFilePath = '/log4j/epicenter/epicenter.log';
 
-var deconstructedLine, lineDate, lineTime, lineTimeTillSeconds, lineTimeTillMilliSeconds, lineLogLevel, lineLogMessage = '';
+var deconstructedLine = '',
+    lineDate = '',
+    lineTime = '',
+    lineTimeTillSeconds = '',
+    lineTimeTillMilliSeconds = '',
+    lineLogLevel = '',
+    lineLogMessage = '',
+    resultObject = '';
 
 mongoClient.connect(mongoDbUrl, function(err, connection) {
 
@@ -29,17 +36,29 @@ mongoClient.connect(mongoDbUrl, function(err, connection) {
 });
 
 function insertIntoDb(connection, lineLogLevel, lineLogMessage, lineTimeTillMilliSeconds, lineTimeTillSeconds, lineDate) {
+    var arr0 = [];
+    arr0[0] = lineLogMessage;
+
     var obj1 = {};
-    obj1[lineLogLevel] = lineLogMessage;
+    obj1[lineLogLevel] = arr0;
+
+    var arr1 = [];
+    arr1[0] = obj1;
 
     var obj2 = {};
-    obj2[lineTimeTillMilliSeconds] = obj1;
+    obj2[lineTimeTillMilliSeconds] = arr1;
+
+    var arr2 = [];
+    arr2[0] = obj2;
 
     var obj3 = {};
-    obj3[lineTimeTillSeconds] = obj2;
+    obj3[lineTimeTillSeconds] = arr2;
+
+    var arr3 = [];
+    arr3[0] = obj3;
 
     var obj4 = {};
-    obj4[lineDate] = obj3;
+    obj4[lineDate] = arr3;
 
     db.insertIntoDB(connection, obj4);
 }
@@ -52,23 +71,83 @@ function extractLineVariables() {
     lineLogLevel = deconstructedLine[2];
     lineLogMessage = deconstructedLine.slice(3).join("");
 }
+
 var doJob = function(connection, lr) {
 
     function doTheInsertingBusiness() {
-        db.doesValueExistInDb(connection, lineDate, function (dateExists) {
+        var o1= {};
+        o1[lineDate] = {$exists: true, $ne: null};
+
+        db.doesValueExistInDb(connection, o1, function (dateExists, result) {
 
             if (dateExists) {
+                resultObject = result;
 
-                db.doesValueExistInDb(connection, lineDate + '.' + lineTimeTillSeconds, function (timeTillSecondsExists) {
+                var o3 = {};
+                o3[lineTimeTillSeconds] = {$exists: true, $ne: null};
+                var o2 = {};
+                o2['$elemMatch'] = o3;
+                var o1 = {};
+                o1[lineDate] = o2;
+
+                db.doesValueExistInDb(connection, o1, function (timeTillSecondsExists, result) {
                     if (timeTillSecondsExists) {
+                        resultObject = result;
 
-                        db.doesValueExistInDb(connection, lineDate + '.' + lineTimeTillSeconds + '.' + lineTimeTillMilliSeconds, function (timeTillMsExists) {
+                        var o5 = {};
+                        o5[lineTimeTillMilliSeconds] = {$exists: true, $ne: null};
+                        var o4 = {};
+                        o4['$elemMatch'] = o5;
+                        var o3 = {};
+                        o3[lineTimeTillSeconds] = o4;
+                        var o2 = {};
+                        o2['$elemMatch'] = o3;
+                        var o1 = {};
+                        o1[lineDate] = o2;
+
+                        db.doesValueExistInDb(connection, o1, function (timeTillMsExists, result) {
                             if (timeTillMsExists) {
+                                resultObject = result;
 
-                                db.doesValueExistInDb(connection, lineDate + '.' + lineTimeTillSeconds + '.' + lineTimeTillMilliSeconds + '.' + lineLogLevel, function (logLevelExists) {
+                                var o7 = {};
+                                o7[lineLogLevel] = {$exists: true, $ne: null};
+                                var o6 = {};
+                                o6['$elemMatch'] = o7;
+                                var o5 = {};
+                                o5[lineTimeTillMilliSeconds] = o6;
+                                var o4 = {};
+                                o4['$elemMatch'] = o5;
+                                var o3 = {};
+                                o3[lineTimeTillSeconds] = o4;
+                                var o2 = {};
+                                o2['$elemMatch'] = o3;
+                                var o1 = {};
+                                o1[lineDate] = o2;
+
+                                db.doesValueExistInDb(connection, o1, function (logLevelExists, result) {
                                     if (logLevelExists) {
+                                        resultObject = result;
 
-                                        db.doesValueExistInDb(connection, lineDate + '.' + lineTimeTillSeconds + '.' + lineTimeTillMilliSeconds + '.' + lineLogLevel + '.' + lineLogMessage, function (logMessageExists) {
+                                        var o9 = [];
+                                        o9[0] = lineLogMessage;
+                                        var o8 = {};
+                                        o8['$in'] = o9;
+                                        var o7 = {};
+                                        o7[lineLogLevel] = o8;
+                                        var o6 = {};
+                                        o6['$elemMatch'] = o7;
+                                        var o5 = {};
+                                        o5[lineTimeTillMilliSeconds] = o6;
+                                        var o4 = {};
+                                        o4['$elemMatch'] = o5;
+                                        var o3 = {};
+                                        o3[lineTimeTillSeconds] = o4;
+                                        var o2 = {};
+                                        o2['$elemMatch'] = o3;
+                                        var o1 = {};
+                                        o1[lineDate] = o2;
+
+                                        db.doesValueExistInDb(connection, o1, function (logMessageExists, result) {
                                             if (logMessageExists) {
                                                 console.log('everything already exists')
                                             }
